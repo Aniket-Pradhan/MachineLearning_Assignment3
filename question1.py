5cc506bb8f6439b7a04bf6cbd8deb2b38f8db305
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-11-28 12:55:15 +0530

    Part d: sklean MLP

--- question1.py
+++ question1.py
@@ -1,10 +1,16 @@
 import os
 import sys
 import pickle
+import argparse
 import numpy as np
 np.seterr(over="ignore")
 from pathlib import Path
 from random import randrange
+import matplotlib.pyplot as plt
+from keras.utils import np_utils
+from sklearn.manifold import TSNE
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 
 from ML_HW3 import datasets
@@ -28,9 +34,6 @@ def storemodel(model, name):
 	print("saved", filepath)
 
 def loadmodel(filename):
-	root = str(Path(__file__).parent.parent)
-	modeldir = root + "/models"
-	filename = modeldir + "/" + filename
 	try:
 		model = loadPickle(filename)
 		return model
@@ -41,21 +44,27 @@ def checkandcreatedir(path):
 	if not os.path.isdir(path):
 		os.makedirs(path)
 
-dataset = datasets.dataset(sys.argv[1])
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True, help="Path to the dataset")
+ap.add_argument("-m1", "--model1", required=False, default="", help="Path to first model (self NN)")
+ap.add_argument("-m2", "--model2", required=False, default="", help="Path to second model (sklearn MLP)")
+args = vars(ap.parse_args())
+
+dataset_path = args["dataset"]
+model1 = args["model1"]
+model2 = args["model2"]
+
+dataset = datasets.dataset(dataset_path)
 data = dataset.loadMNIST()
 x_train, x_test, y_train, y_test = train_test_split(data["X"], data["Y"], test_size=0.2)
 x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size=0.2)
 
-model_dir = -1
-if len(sys.argv) == 3:
-	model_dir = sys.argv[2]
-
-if model_dir == -1:
-	nn = neuralnetwork.NN(x_train, y_train, num_layers = 3, num_neurons = [100, 50, 50], epochs=5)
+if model1 == "":
+	nn = neuralnetwork.NN(x_train, y_train, num_layers = 3, num_neurons = [100, 50, 50], epochs=1)
 	nn.train()
 	storemodel(nn, "nn_model")
 else:
-	nn = loadmodel("nn_model")
+	nn = loadmodel(model1)
 
 correct_pred = 0
 for x, y in zip(x_test, y_test):
@@ -64,3 +73,38 @@ for x, y in zip(x_test, y_test):
 		correct_pred += 1
 # print accuracy
 print("Accuracy:", correct_pred/x_test.shape[0])
+
+## Part c
+# tsne = TSNE(n_components=2, verbose = 1)
+# tsne_results = tsne.fit_transform(nn.W[-1])
+# print(tsne_results.T)
+
+# y_test = [0 if i == 7 else 1 for i in y_test]
+# y_test = np.array(y_test)
+
+# y_test_cat = np_utils.to_categorical(y_test, num_classes = 2)
+# color_map = np.argmax(y_test_cat, axis=1)
+# plt.figure(figsize=(10,10))
+# for cl in range(2):
+# 	indices = np.where(color_map==cl)
+# 	indices = indices[0]
+# 	print(indices, tsne_results.shape)
+# 	plt.scatter(tsne_results[indices,0], tsne_results[indices, 1], label=cl)
+# plt.legend()
+# plt.show()
+
+## Part d
+x_train = np.reshape(x_train, (len(x_train), 28*28))
+x_test = np.reshape(x_test, (len(x_test), 28*28))
+
+if model2 == "":
+	clf = MLPClassifier(solver='sgd', activation="logistic", hidden_layer_sizes=(100, 50, 50), learning_rate_init=0.1, max_iter=1)
+	clf.fit(x_train, y_train)
+	storemodel(clf, "sklean_mlp")
+else:
+	clf = loadmodel(model2)
+
+correct_pred = 0
+pred = clf.predict(x_test)
+accuracy = accuracy_score(y_test, pred)
+print("Accuracy:", accuracy)
