a8ceb8cacfeeedccefe462ed97859b8b416694b7
Aniket Pradhan <aniket17133@iiitd.ac.in>
2019-11-29 15:03:26 +0530

    Renamed file

--- question2.py
+++ question2.py
@@ -1,150 +1,141 @@
-import torch.optim as optim
-import torch.utils.data
-import torch.backends.cudnn as cudnn
-import torchvision
-from torchvision import transforms as transforms
-import numpy as np
-
+import os
+import pickle
 import argparse
+import numpy as np
+import progressbar
+from pathlib import Path
+import matplotlib.pyplot as plt
 
-from ML_HW3 import cifar10
-from misc import progress_bar
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import label_binarize
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import roc_curve, auc, accuracy_score, confusion_matrix
 
-CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+def savePickle(filename, data):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
 
-def main():
-    parser = argparse.ArgumentParser(description="cifar-10 with PyTorch")
-    parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
-    parser.add_argument('--epoch', default=200, type=int, help='number of epochs tp train for')
-    parser.add_argument('--trainBatchSize', default=100, type=int, help='training batch size')
-    parser.add_argument('--testBatchSize', default=100, type=int, help='testing batch size')
-    parser.add_argument('--cuda', default=torch.cuda.is_available(), type=bool, help='whether cuda is in use')
-    args = parser.parse_args()
+def loadPickle(filename):
+    with open(filename, 'rb') as handle:
+        b = pickle.load(handle)
+    return b
 
-    solver = Solver(args)
-    solver.run()
+def storemodel(model, name):
+    root = str(Path(__file__).parent.parent)
+    modeldir = root + "/models"
+    checkandcreatedir(modeldir)
+    filepath = modeldir + "/" + name
+    savePickle(filepath, model)
+    print("saved", filepath)
 
+def loadmodel(filename):
+    try:
+        model = loadPickle(filename)
+        return model
+    except:
+        raise Exception("Model not found: " + filename )
 
-class Solver(object):
-    def __init__(self, config):
-        self.model = None
-        self.lr = config.lr
-        self.epochs = config.epoch
-        self.train_batch_size = config.trainBatchSize
-        self.test_batch_size = config.testBatchSize
-        self.criterion = None
-        self.optimizer = None
-        self.scheduler = None
-        self.device = None
-        self.cuda = config.cuda
-        self.train_loader = None
-        self.test_loader = None
+def checkandcreatedir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
 
-    def load_data(self):
-        train_transform = transforms.Compose([transforms.RandomHorizontalFlip(), transforms.ToTensor()])
-        test_transform = transforms.Compose([transforms.ToTensor()])
-        train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
-        self.train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=self.train_batch_size, shuffle=True)
-        test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
-        self.test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=self.test_batch_size, shuffle=False)
+def plot_confusion_matrix(y_true, y_pred, classes, normalize=False, title=None, cmap=plt.cm.PuRd):
+    cm = confusion_matrix(y_true, y_pred)
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
 
-    def load_model(self):
-        if self.cuda:
-            self.device = torch.device('cuda')
-            cudnn.benchmark = True
-        else:
-            self.device = torch.device('cpu')
+test = 1
+storemodel(test, "test_model")
 
-        # self.model = LeNet().to(self.device)
-        # self.model = AlexNet().to(self.device)
-        # self.model = VGG11().to(self.device)
-        # self.model = VGG13().to(self.device)
-        # self.model = VGG16().to(self.device)
-        # self.model = VGG19().to(self.device)
-        # self.model = GoogLeNet().to(self.device)
-        # self.model = resnet18().to(self.device)
-        # self.model = resnet34().to(self.device)
-        # self.model = resnet50().to(self.device)
-        # self.model = resnet101().to(self.device)
-        # self.model = resnet152().to(self.device)
-        # self.model = DenseNet121().to(self.device)
-        # self.model = DenseNet161().to(self.device)
-        # self.model = DenseNet169().to(self.device)
-        # self.model = DenseNet201().to(self.device)
-        self.model = WideResNet(depth=28, num_classes=10).to(self.device)
+ap = argparse.ArgumentParser()
+ap.add_argument("-t1", "--train", required=True, help="Path to the train features")
+ap.add_argument("-t2", "--test", required=True, help="Path to the test features")
+ap.add_argument("-m", "--model", required=False, default="", help="Path to the train features")
+args = ap.parse_args()
 
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
-        self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[75, 150], gamma=0.5)
-        self.criterion = nn.CrossEntropyLoss().to(self.device)
+train_features_path = args.train
+test_features_path = args.test
+model_dir = args.model if args.model != "" else False
 
-    def train(self):
-        print("train:")
-        self.model.train()
-        train_loss = 0
-        train_correct = 0
-        total = 0
+## Load data
+if not model_dir:
+    train_features = loadmodel(train_features_path)
+    train_x = train_features["X"]
+    train_y = train_features["Y"]
+    train_x = np.reshape(train_x, (len(train_x), 1000))
 
-        for batch_num, (data, target) in enumerate(self.train_loader):
-            data, target = data.to(self.device), target.to(self.device)
-            self.optimizer.zero_grad()
-            output = self.model(data)
-            loss = self.criterion(output, target)
-            loss.backward()
-            self.optimizer.step()
-            train_loss += loss.item()
-            prediction = torch.max(output, 1)  # second param "1" represents the dimension to be reduced
-            total += target.size(0)
+test_features = loadmodel(test_features_path)
+test_x = test_features["X"]
+test_y = test_features["Y"]
+test_x = np.reshape(test_x, (len(test_x), 1000))
+class_names = np.array(list(set(test_y)))
+n_classes = len(class_names)
 
-            # train_correct incremented by one if predicted right
-            train_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
+## Train
+if not model_dir:
+    print("Training")
+    clf = LinearSVC(random_state=0, tol=1e-5)
+    clf.fit(train_x, train_y)
+    storemodel(clf, "cifar_svc")
+else:
+    clf = loadmodel(model_dir)
 
-            progress_bar(batch_num, len(self.train_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
-                         % (train_loss / (batch_num + 1), 100. * train_correct / total, train_correct, total))
+## Test
+print("Predicting")
+preds = clf.predict(test_x)
 
-        return train_loss, train_correct / total
+## Get metrics
+# Accuracy
+accuracy = accuracy_score(test_y, preds)
+print("Accuracy:", accuracy)
 
-    def test(self):
-        print("test:")
-        self.model.eval()
-        test_loss = 0
-        test_correct = 0
-        total = 0
+# Confusion Matrix
+# confmat = confusion_matrix(test_y, preds)
+# plt.figure()
+plot_confusion_matrix(test_y, preds, classes=class_names, normalize=True, title='Normalized confusion matrix')
 
-        with torch.no_grad():
-            for batch_num, (data, target) in enumerate(self.test_loader):
-                data, target = data.to(self.device), target.to(self.device)
-                output = self.model(data)
-                loss = self.criterion(output, target)
-                test_loss += loss.item()
-                prediction = torch.max(output, 1)
-                total += target.size(0)
-                test_correct += np.sum(prediction[1].cpu().numpy() == target.cpu().numpy())
+# ROC Curve
+# Compute ROC curve and ROC area for each class
+test_y = np.reshape(test_y, (len(test_y), 1))
+preds = np.reshape(preds, (len(preds), 1))
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+fpr[0], tpr[0], _ = roc_curve(test_y[:, 0], preds[:, 0])
+roc_auc[0] = auc(fpr[0], tpr[0])
 
-                progress_bar(batch_num, len(self.test_loader), 'Loss: %.4f | Acc: %.3f%% (%d/%d)'
-                             % (test_loss / (batch_num + 1), 100. * test_correct / total, test_correct, total))
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(test_y.ravel(), preds.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
-        return test_loss, test_correct / total
-
-    def save(self):
-        model_out_path = "model.pth"
-        torch.save(self.model, model_out_path)
-        print("Checkpoint saved to {}".format(model_out_path))
-
-    def run(self):
-        self.load_data()
-        self.load_model()
-        accuracy = 0
-        for epoch in range(1, self.epochs + 1):
-            self.scheduler.step(epoch)
-            print("\n===> epoch: %d/200" % epoch)
-            train_result = self.train()
-            print(train_result)
-            test_result = self.test()
-            accuracy = max(accuracy, test_result[1])
-            if epoch == self.epochs:
-                print("===> BEST ACC. PERFORMANCE: %.3f%%" % (accuracy * 100))
-                self.save()
-
-
-if __name__ == '__main__':
-    main()
+plt.figure()
+lw = 1
+plt.plot(fpr[0], tpr[0], color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[0])
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.legend()
+plt.show()
